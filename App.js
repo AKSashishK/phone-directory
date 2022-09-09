3a910ac77a8facb6a1037848fe6d34f62ac6f8ad
Ashish Kumar Singh <63630929+AKSashishK@users.noreply.github.com>
2022-09-09 09:05:50 +0530

    Created skeleton for the homepage

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import React, {Component} from "react";
+import React from "react";
 
 function App() {
   return (
     <div>
-      
+      {/* <div className='header'>
+        Phone Directory
+      </div>
+      <button> Add </button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div> */}
+      <label htmlFor="name">Name</label>
+      <input id="name" type="text" placeholder="Type here" defaultValue="Ashish" />
     </div>
   );
 }
